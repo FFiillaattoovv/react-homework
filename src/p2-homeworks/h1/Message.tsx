df362c9451882343d27e17b6471cbc663fe345f3
FFiillaattoovv <admirator2015@yandex.ru>
2021-08-24 18:35:46 +0300

    Add completed homework #1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import classes from './Message.module.css'
 
-function Message() {
-    return (
-        <div>
 
-        </div>
-    )
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
 }
 
-export default Message
+const Message = (props: MessagePropsType) => {
+    return (
+        <div className={classes.post}>
+            <img src={props.avatar} alt="Avatar"/>
+            <div className={classes.message}>
+                <div className={classes.name}>{props.name}</div>
+                <div className={classes.text}>{props.message}</div>
+                <div className={classes.time}>{props.time}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Message;
