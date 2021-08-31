8bdbb3f95b3d65d823d932a4c412838951fadded
FFiillaattoovv <admirator2015@yandex.ru>
2021-08-31 12:37:27 +0300

    Add completed homework #8

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import HW7 from '../../h7/HW7';
+import HW8 from '../../h8/HW8';
 
 function PreJunior() {
     return (
         <div>
             <h1>JUNIOR</h1>
             <HW7/>
-            {/*<HW8/>*/}
+            <HW8/>
             {/*<HW9/>*/}
             {/*<HW10/>*/}
             {/*<HW11/>*/}
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,41 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+
+const initialState: Array<UserType> = []
+
+export const homeWorkReducer = (state: Array<UserType> = initialState, action: ActionsType): Array<UserType> => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload === 'up') {
+                return state.slice().sort((a, b) => {
+                    if (a.age < b.age) return 1
+                    if (a.age > b.age) return -1
+                    return 0
+                })
+            } else if (action.payload === 'down') {
+                return state.slice().sort((a, b) => {
+                    if (a.age > b.age) return 1
+                    if (a.age < b.age) return -1
+                    return 0
+                })
+            } else {
+                return state
+            }
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(u => u.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
+
+
+export const sortPeopleAC = (direction: SortingDirectionType) => ({type: 'sort', payload: direction} as const)
+export const checkPeopleAC = (age: number) => ({type: 'check', payload: age} as const)
+
+
+type ActionsType = ReturnType<typeof sortPeopleAC> | ReturnType<typeof checkPeopleAC>
+type SortingDirectionType = 'up' | 'down'
+
+
